1f003813678e2668fa2dcbd49a8ee876588b2160
Angel Ruiz <aruizca@gmail.com>
2015-02-18 13:06:14 +1100

    Added support to Ajax request queueing

--- grails-app/conf/AjaxAnywhereResources.groovy
+++ grails-app/conf/AjaxAnywhereResources.groovy
@@ -2,9 +2,9 @@ modules = {
 
     ajaxanywhere {
         dependsOn 'jquery'
-        
+
+        resource url:[plugin: 'ajaxanywhere', dir: 'js',file:'jquery-ajaxq-0.0.2.js']
         resource url:[plugin: 'ajaxanywhere', dir: 'js',file:'jquery-aa.js']
-        
     }
     
 }
