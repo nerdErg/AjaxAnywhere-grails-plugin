80f289c746b9946ee92fd35ac739bad0d859d1b3
Burt Beckwith <burt@burtbeckwith.com>
2015-05-11 22:55:25 -0400

    cleanup

--- AjaxanywhereGrailsPlugin.groovy
+++ AjaxanywhereGrailsPlugin.groovy
@@ -22,26 +22,18 @@ import com.nerderg.ajaxanywhere.listener.JulToSlf4jBridgeListener
 class AjaxanywhereGrailsPlugin {
     def version = "1.0-SNAPSHOT"
     def grailsVersion = "1.3.x > *"
-    def pluginExcludes = [
-        "web-app/WEB-INF/**"
-    ]
-
     def title = "AjaxAnywhere Grails Plugin"
-    def description = '''\\
-This plugin integrates the <a href="http://www.ajaxanywhere.com" target="_AjaxAnywhere">"AjaxAnywhere (Reloaded) library</a> seamlessly inside the Grails Framework ecosystem.
-'''
-
+    def description = 'Integrates the <a href="http://www.ajaxanywhere.com" target="_AjaxAnywhere">"AjaxAnywhere (Reloaded) library</a> seamlessly.'
     def documentation = "http://ajaxanywhere.com/documentation.html"
     def license = "APACHE"
     def organization = [ name: "nerdErg Pty. Ltd.", url: "http://www.nerderg.com/" ]
-    def author = "Angel Ruiz"
-    def authorEmail = "aruizca@gmail.com"
     def developers = [
         [ name: "Angel Ruiz", email: "aruizca@gmail.com" ]
     ]
     def issueManagement = [ system: "GitHub", url: "https://github.com/nerdErg/AjaxAnywhere-grails-plugin/issues" ]
     def scm = [ url: "https://github.com/nerdErg/AjaxAnywhere-grails-plugin" ]
     def loadAfter = ['resources']
+
     def doWithWebDescriptor = { webXml ->
 
         // AjaxAnywhere filter mapping
@@ -64,24 +56,9 @@ This plugin integrates the <a href="http://www.ajaxanywhere.com" target="_AjaxAn
         // Register listener to bridge AjaxAnywhere JUL based traces to SLF4J
         def contextParam = webXml.'context-param'
         contextParam[contextParam.size() - 1] + {
-            'listener' {
+            listener {
                 'listener-class'(JulToSlf4jBridgeListener.name)
             }
         }
-    }
-
-    def doWithSpring = {
-    }
-
-    def doWithDynamicMethods = { ctx ->
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-    }
-
-    def onChange = { event ->
-    }
-
-    def onConfigChange = { event ->
     }
 }
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,31 +1,27 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.work.dir = 'target'
 
-grails.project.dependency.resolver = "maven" // or ivy
+grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+
+    inherits 'global'
+    log 'warn'
+
     repositories {
         mavenLocal()
         grailsCentral()
         mavenCentral()
+
         mavenRepo("http://nexus.ala.org.au/content/groups/public/") {
             updatePolicy 'always'
         }
     }
+
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         compile 'com.nerderg.ajaxanywhere:ajaxanywhere:2.1'
     }
+
     plugins {
-        build(":release:3.0.1",
-                ":rest-client-builder:2.0.3") {
+        build(":release:3.0.1", ":rest-client-builder:2.0.3") {
             export = false
         }
 
